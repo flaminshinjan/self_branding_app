c6e9545b9f9213860a21db4eb09be2a294a0bb59
flaminshinjan <srikpatra.2008@gmail.com>
2022-12-26 02:59:38 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/self_branding_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.self_branding_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/projects.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyProject extends StatefulWidget {
+  const MyProject({Key? key}) : super(key: key);
+
+  @override
+  State<MyProject> createState() => _MyProjectState();
+}
+
+class _MyProjectState extends State<MyProject> {
+  projetCard(lang, title, description, star) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 220,
+      child: Card(
+        child: Container(
+          margin: EdgeInsets.only(left: 20, top: 30, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                lang,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                description,
+                style: TextStyle(color: Colors.white70, fontSize: 16),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    star,
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        FontAwesomeIcons.github,
+                        color: Colors.white,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+        color: Color(0xff262628),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Color(0xff1e1e1e),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Color(0xff252525),
+        title: Text('Projects'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 20, top: 10),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              projetCard('FLUTTER', 'Whatsapp UI Clone',
+                  'UI of a messenger app', '10'),
+              projetCard('SwiftUI', 'MeditationApp',
+                  'App for playing meditation music.', '9'),
+              projetCard(
+                  'PYTHON', 'Tic Tac Toe Game', 'A basic gaming app.', '8'),
+              projetCard('SwiftUI', 'TipsCalculator',
+                  'For calculating tips between friends.', '7'),
+              projetCard('SwiftUI', 'Weather App',
+                  'A app to display the weather of a particular area', '10'),
+              projetCard('SwiftUI', 'ExpenseTracker',
+                  'To track and analyse expenses of one', '7')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
